5befacf3664bbf5edec6a9af41f5532be563d4e2
David Domkář <dave.domkar@gmail.com>
2021-04-12 22:02:26 +0200

    feat: add source

---
+++ android/app/src/main/kotlin/com/example/beamer_beam_back_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beamer_beam_back_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final routerDelegate = BeamerRouterDelegate(
+    locationBuilder: BeamerLocationBuilder(
+      beamLocations: [
+        RootLocation(),
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      routeInformationParser: BeamerRouteInformationParser(),
+      routerDelegate: routerDelegate,
+      backButtonDispatcher: BeamerBackButtonDispatcher(delegate: routerDelegate),
+    );
+  }
+}
+
+class RootLocation extends BeamLocation {
+  final _beamerKey = GlobalKey<BeamerState>();
+
+  @override
+  List<String> get pathBlueprints => ['/*'];
+
+  @override
+  List<BeamPage> pagesBuilder(BuildContext? context, BeamState state) {
+    return [
+      BeamPage(
+        key: const ValueKey('home'),
+        child: Beamer(
+          key: _beamerKey,
+          routerDelegate: BeamerRouterDelegate(
+            locationBuilder: BeamerLocationBuilder(
+              beamLocations: [
+                HomeLocation(),
+                SettingsLocation(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ];
+  }
+}
+
+class HomeLocation extends BeamLocation {
+  @override
+  List<String> get pathBlueprints => [
+        '/',
+      ];
+
+  @override
+  List<BeamPage> pagesBuilder(BuildContext context, BeamState state) {
+    return [
+      BeamPage(
+        key: const ValueKey('home'),
+        child: HomeScreen(),
+      ),
+    ];
+  }
+}
+
+class SettingsLocation extends BeamLocation {
+  @override
+  List<String> get pathBlueprints => [
+        '/settings',
+      ];
+
+  @override
+  List<BeamPage> pagesBuilder(BuildContext context, BeamState state) {
+    return [
+      BeamPage(
+        key: const ValueKey('settings'),
+        child: SettingsScreen(),
+      ),
+    ];
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          context.beamToNamed('/settings');
+        },
+        child: Text('settings'),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          context.beamBack();
+        },
+        child: Text('beam back'),
+      ),
+    );
+  }
+}
